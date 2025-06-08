c48c44cd236ac5089174f06649f92871219ae658
JamesVictor-o <victorjames40v@gmail.com>
2025-06-09 00:32:41 +0100

    understanding struct

--- src/main.rs
+++ src/main.rs
@@ -32,21 +32,53 @@
 
 // slice
 
-fn main(){
-    let s=String::from("Hello, world!");  
-    let slice=first_word(&s);
+// fn main(){
+//     let s=String::from("Hello, world!");  
+//     let slice=first_word(&s);
 
-    println!("The first word is: {}, {}",s, slice);
+//     println!("The first word is: {}, {}",s, slice);
+// }
+
+
+// fn first_word(s: &String)-> &str{
+//         let bytes= s.as_bytes();
+//         for(i, &items) in bytes.iter().enumerate(){
+//                 if items == b' ' {
+//                       return &s[0..i];
+//                 }
+//         }
+
+//         &s[..]
+// }
+
+// struct
+
+struct User{
+       username:String,
+       email:String,
+       sign_in_count: u64,
+       active: bool,
 }
 
+fn main (){
+       let user1= build_user(String::from("victorjames408@gmail.com"), String::from("jamesvictor408"));
 
-fn first_word(s: &String)-> &str{
-        let bytes= s.as_bytes();
-        for(i, &items) in bytes.iter().enumerate(){
-                if items == b' ' {
-                      return &s[0..i];
-                }
+       let user2: User = User {
+              email: String::from("victorjames408@gmail.com"),
+              username: String::from("jamesvictor408"),
+              ..user1
+       };
+
+       println!("User email: {}, username: {}, sign_in_count: {}, active: {}", 
+                user1.email, user1.username, user1.sign_in_count, user1.active);
+}
+ 
+
+fn build_user(email:String, username:String)-> User{
+       User {
+              email,
+              username,
+              sign_in_count: 1,
+              active: true,
         }
-
-        &s[..]
 }
